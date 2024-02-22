5fd9533bea68190cbfc020f36c3a14adc2b81ff3
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-21 20:42:28 -0500

    phase-1-lecture-videos-javascript-conditionals/
    phase-1-lecture-videos-javascript-conditionals/

--- JavaScriptConditionals.js
+++ JavaScriptConditionals.js
@@ -3,13 +3,30 @@ function isRose(cat) {
 }
 
 
-function isRose(cat) {
+function isDutchess(cat) {
    switch (cat) {
-      case 'rose' :
-          return 'Hi rose you are cute!'
+      case 'Dutchess' :
+    return 'Hi Dutchess you are cute!'
       case 'ted':
          return 'Hi ted nice to see you !'
       default:
          return `Hi ${cat}!`
    }
+}
+
+
+
+function isDune(cat) {
+  return cat === 'rose' ? 'Hi Rose you are cute!' : `Hi ${cat}!`;
+}
+
+function isChuckles(cat) {
+  switch (cat) {
+    case 'Dutchess':
+      return 'Hi Dutchess you are cute!';
+    case 'ted':
+      return 'Hi ted nice to see you!';
+    default:
+      return `Hi ${cat}!`;
+  }
 }
